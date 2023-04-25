f2da50cd769914d0894db9f930112c69f287a6e6
Vasili Karpenkin <v.karpenkin@gmail.com>
2023-04-25 23:33:04 +0300

    0.5.1 Formik validate

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -8,31 +8,48 @@ export const Login = () => {
         initialValues: {
             Email: '',
             Password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            let errors: any = {}
+            if (!values.Email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.Email)) {
+                errors.email = 'Invalid email address'
+            }
+            return errors
         },
         onSubmit: values => {
             console.log(values)
         }
     })
-    console.log(formik.values)
+    console.log(formik.errors)
     return (
-        <div>
-        <h1>Login</h1>
-            <form>
-                <SuperInput
-                    onChange={formik.handleChange}
-                    value={formik.values.Email}
-                    name='Email'
-                    error={''}
-                />
-                <SuperInput
-                    onChange={formik.handleChange}
-                    value={formik.values.Password}
-                    name='Password'
-                    type='password'
-                    error={''}
-                />
-            </form>
-        </div>
+        <form onSubmit={formik.handleSubmit}>
+            <h1>Login</h1>
+            <SuperInput
+                onChange={formik.handleChange}
+                value={formik.values.Email}
+                name='Email'
+                error={''}
+            />
+            <SuperInput
+                onChange={formik.handleChange}
+                value={formik.values.Password}
+                name='Password'
+                type='password'
+                error={''}
+            />
+            <input
+                onChange={formik.handleChange}
+                checked={formik.values.rememberMe}
+                name='rememberMe'
+                type='checkbox'
+            />
+            <button type='submit'>
+                Submit
+            </button>
+        </form>
     );
 };
 
